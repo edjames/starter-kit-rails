1ad6645e75da58c336f637f5568bcacf5b8d3e4b
Ed James <ed.james.email@gmail.com>
2019-09-26 21:33:05 +0100

    (NT) Fixing jQuery loading issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,24 @@
-/* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+require("jquery")
+require("bootstrap/dist/js/bootstrap.bundle.min")
+require("jquery-datetimepicker/build/jquery.datetimepicker.full")
+require("moment/moment")
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("channels")
+require("channels/consumer")
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+// Uncomment this line to verify that webpacker is working.
+// console.log('Hello World from Webpacker')
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack');
+
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery'
+}));
+
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
 module.exports = environment
