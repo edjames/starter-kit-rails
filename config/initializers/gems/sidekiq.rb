1a63655d73084286c99098b25ae2a3249fe0b00d
Ed James <ed.james.email@gmail.com>
2022-05-15 14:38:56 +0100

    (NT) Fix broken Gemfile.lock; Remove deprecation warning

--- config/initializers/gems/sidekiq.rb
+++ config/initializers/gems/sidekiq.rb
@@ -1,7 +1,5 @@
 require 'sidekiq/web'
 
-Sidekiq::Web.set :sessions, false
-
 Sidekiq.configure_server do |config|
   log_level = ENV.fetch('WORKER_LOG_LEVEL', 'warn').upcase
   config.logger.level = "Logger::#{log_level}".constantize
