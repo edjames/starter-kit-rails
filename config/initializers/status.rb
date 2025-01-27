85cc7c12bfbb4c7e956dbfc8ec6f904350aaafeb
Ed James <ed.j@inductionhealthcare.com>
2025-01-27 08:36:12 +0000

    (NT) Gem updates; Docker image version updates

--- config/initializers/status.rb
+++ config/initializers/status.rb
@@ -2,7 +2,7 @@ StarterKit::BOOT_TIME = Time.now.utc
 
 file = File.join(Rails.root, 'VERSION')
 
-StarterKit::VERSION = if File.exists?(file)
+StarterKit::VERSION = if File.exist?(file)
   File.read(file).strip!
 else
   'development build'
